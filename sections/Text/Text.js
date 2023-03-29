52c8a118789ca5631909640499923c1f80953b01
Ritik732 <guptaritik23364@gmail.com>
2023-03-29 11:32:09 +0530

    first commit

---
+++ sections/Text/Text.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Text = ({headline, description}) => {
+  return (
+    <div>
+        Static Text..
+        <h1>{headline}</h1>
+        <h3>{description}</h3>
+        </div>
+  )
+}
+
+export default Text
